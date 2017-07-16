cd78d172e3e0b995879cb3b70b8dbf611d8d70bb
hoangfeng <hoangfeng2608@gmail.com>
2017-07-16 23:20:16 +0700

    correct analog data

--- User/HoangPM/final arduino/socket-server2/socket-server2.ino
+++ User/HoangPM/final arduino/socket-server2/socket-server2.ino
@@ -2,33 +2,54 @@
 const byte RX = 3;          // Chân 3 được dùng làm chân RX
 const byte TX = 2;          // Chân 2 được dùng làm chân TX
 SoftwareSerial mySerial = SoftwareSerial(RX, TX); 
+int r3,l3, phanhtay,pedantrai,pedanphai,volang;
+boolean mode_status= true;
+boolean handbreak_status = false;
+int m_pedantrai,m_pedanphai,m_volang;
+
 void setup() {
   Serial.begin(115200);
   mySerial.begin(115200); 
+  //pinMode(11, INPUT);
+    
 }
 void loop() { 
-   int volang = analogRead(A5); 
-   int pedantrai =analogRead(A4);
-   int pedanphai = analogRead(A3);  
-   int phanhtay = analogRead(A2);
-   int l3 =analogRead(A1);
-   int r3  = analogRead(A0);
-                                           
-   
-//   Serial.print("pedan trai :");
-//   Serial.println(pedantrai);                
-//   Serial.print("pedan phai :");
-//   Serial.println(pedanphai);                
-   // Serial.print("phanh tay :");
-   // Serial.println(phanhtay);                
-   // Serial.print(" l3  :");
-   // Serial.println(l3); 
-   // Serial.print(" r3  :");
-   // Serial.println(r3);               
-   // Serial.println();
-
-   
-   mySerial.print("{'speed':'"+String(pedanphai)+"','angle':'"+String(volang)+"','break':'"+String(pedantrai)+"','mode1':'"+String(l3)+"','mode2':'"+String(r3)+"','handbreak':'"+String(phanhtay)+"'} \n");
-   delay(500);           //đợi 0.2 giây
+    volang = analogRead(A5); 
+    pedanphai =analogRead(A4);
+    pedantrai= analogRead(A3);  
+    phanhtay = analogRead(A2);
+    l3 =analogRead(A1);
+    r3  = analogRead(A0);
+    //xu ly tin hieu mode
+    if( r3 < 300 && l3 ==1023){
+       mode_status =false;       
+    }   
+     if( l3 == 0 && r3 ==1023){
+       mode_status =true;       
+    }   
+   //handbreak
+     if( (l3 == 1023 || r3 ==1023 )&& phanhtay == 0){
+       handbreak_status =true;       
+    }else{
+      handbreak_status =false;  
+    }
+    m_pedantrai = map(pedantrai,515,1015,0,100);
+    // speed 0-10
+    m_pedanphai = map(pedanphai,565,1015,0,10);
+    if(volang<1015){
+       volang =1015;
+    }
+    // angle 90 - 270
+    if(volang == 1019 || volang == 1018){
+      m_volang =90;
+    }else{
+      m_volang = map(volang,1015,1023,90,270);  
+    }             
+   //Serial.println(",");
+   String data = "{'speed':"+String(m_pedanphai)+",'angle':"+String(m_volang)+",'break':"+String(m_pedantrai)+",'mode':"+String(mode_status)+",'hbreak':"+String(handbreak_status)+"} \n";
+   Serial.println(data);
+   mySerial.print(data);
+   //mySerial.print("{'speed':111,'angle':222,'break':333,'mode1':444,'mode2':555,'handbreak':666}\n");
+   delay(10);           //đợi 0.2 giây
 }
   
